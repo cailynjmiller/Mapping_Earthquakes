ee4e3329b7dbd30c4bc32e9966efc8c021f588c4
Cailyn Miller <cailynmiller@Cailyns-MacBook-Air.local>
2021-03-30 17:08:29 -0500

    13.4.2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -13,7 +13,7 @@ cityData.forEach(function(city) {
   L.circleMarker(city.location, {
     radius: city.population/100000,
     color: "black",
-    fillColor: "#ffffa1"
+    fillColor: "#ffa500"
   })
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 .addTo(map);
